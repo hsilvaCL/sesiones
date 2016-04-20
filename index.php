c843a7243015c7f97a4e4e23fa763b99f1c80bc4
cetecom <cetecom@LC08L1300>
2016-04-20 20:22:19 -0300

    Cambios AJAX

--- index.php
+++ index.php
@@ -4,15 +4,33 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <body>
         <div>
             <form method="POST" action="agregar.php">
                 <input name="texto" id="texto" type="text">
-                <input type="submit" value="Enviar">
+                <input type="button" value="Enviar" onclick="enviotexto()">
             </form>
             
             <br>
+            <div id="resultado"></div>
             <a href="datos.php">Listado</a>
         </div>
+        
+         <script type="text/javascript">
+            function enviotexto(){
+                var texto = $("#texto").val();
+                                
+                 /*Uso de función para caracter especial*/
+                $.ajax({
+                    url:'AgregaAjax.php',
+                    type:'POST',
+                    data:"texto="+texto,
+                    success:function(datos){
+                       $("#resultado").html(datos);
+                    }
+                });
+            }
+          </script>
     </body>
 </html>
